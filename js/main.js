126230ad54aae7b32162237a7e9c335350c64864
quillahava <voiaa304@gmail.com>
2021-11-20 16:11:52 +0300

    format files

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
-(function(){
-    const header = document.querySelector('.header');
-    window.onscroll = () => {
-        if(window.pageYOffset > 150){
-            header.classList.add('header__active');
-        } else{
-            header.classList.remove('header__active');
-        }
-    };
-}());
+(function () {
+  const header = document.querySelector(".header");
+  window.onscroll = () => {
+    if (window.pageYOffset > 150) {
+      header.classList.add("header__active");
+    } else {
+      header.classList.remove("header__active");
+    }
+  };
+})();
 
 // Burger handler
 
-(function (){
-    const burgerItem = document.querySelector('.burger');
-    const menu = document.querySelector('.header__nav');
-    const closeButton = document.querySelector('.header__nav-close');
-    burgerItem.addEventListener('click', () => {
-        menu.classList.add('header__nav_active');
-    });
-    closeButton.addEventListener('click', () => {
-        menu.classList.remove('header__nav_active');
-    })
-}());
+(function () {
+  const burgerItem = document.querySelector(".burger");
+  const menu = document.querySelector(".header__nav");
+  const closeButton = document.querySelector(".header__nav-close");
+  burgerItem.addEventListener("click", () => {
+    menu.classList.add("header__nav_active");
+  });
+  closeButton.addEventListener("click", () => {
+    menu.classList.remove("header__nav_active");
+  });
+})();
